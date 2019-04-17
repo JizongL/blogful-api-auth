098aa05f064751710f7b51aadc64664028677168
JizongL <2015rpro@gmail.com>
2019-04-17 18:36:55 -0400

    added compare password and validate user and password test

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -1,9 +1,16 @@
+const bcrypt = require('bcryptjs')
+
 const AuthService = {
   getUserWithUserName(db, user_name) {
     return db('blogful_users')
       .where({ user_name })
       .first()
   },
+  
+  comparePasswords(password,hash){
+    return bcrypt.compare(password,hash)
+  },
+
   parseBasicToken(token) {
     return Buffer
       .from(token, 'base64')
